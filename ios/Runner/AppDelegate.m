7893cca2bccdf5aefc0f6e2f9e040e0b10adac3c
zengtingwei <u6225609@anu.edu.au>
2019-05-05 16:43:47 +1000

    set map environment for ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -11,10 +11,4 @@
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
-//https://pub.dartlang.org/packages/simple_auth_flutter#-readme-tab-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation{
-    return [SimpleAuthFlutterPlugin checkUrl:url];
-}
-
 @end
