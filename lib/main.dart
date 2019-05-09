bd3c6ad462e9f4bcf65eed2df6bcc6362cbd6cb4
zengtingwei <u6225609@anu.edu.au>
2019-05-09 12:50:08 +1000

    create a card widget for now

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:eatwithme/pages/intro/splash_screen.dart';
 import 'package:eatwithme/pages/intro/intro_screen.dart';
 import 'package:eatwithme/utils/matchFriends.dart';
 import 'package:eatwithme/map/map.dart';
+import 'package:eatwithme/map/visitingCard.dart';
 
 
 void main() => runApp(new MyApp());
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
       title: APP_TITLE,
       theme: themeLight(),
       routes: routes,
-      home: SplashScreen(),
+      home: visitingCard(),
     );
   }
 }
--- lib/map/visitingCard.dart
+++ lib/map/visitingCard.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 class visitingCard extends StatefulWidget{
   @override
@@ -10,19 +9,50 @@ class visitingCard extends StatefulWidget{
 
 class _animationCard extends State<visitingCard> with TickerProviderStateMixin{
 
-  Future<void> getInformation(String name) async{
-    String displayName;
-    List<String> interest;
-    QuerySnapshot db = await Firestore.instance.collection("Users").where("displayName", isEqualTo: name).getDocuments();
-    var list = db.documents;
-    list.forEach((DocumentSnapshot ds) => {
-      displayName = ds.data['displayName'],
-      interest = ds.data['interests'],
-    });
+  String displayName = "";
+  List<dynamic> interestss;
+  List<dynamic> interests = [];
+
+  Widget buildCard(displayName, interests){
+    Widget card = new Card(
+      child: new Column(
+        children: <Widget>[
+          new ListTile(
+            onTap: () => {},
+            onLongPress: () => {},
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage('https://i.stack.imgur.com/Dw6f7.png'),
+            ),
+            title: Text(displayName),
+            subtitle: Text(interests.toString()),
+          )
+        ],
+      ),
+    );
+    return card;
   }
+
+Widget _buildBody(BuildContext context, String name) {
+ return StreamBuilder<QuerySnapshot>(
+   stream: Firestore.instance.collection('Users').where("displayName", isEqualTo: name).snapshots(),
+   builder: (context, snapshot) {
+     if (!snapshot.hasData) return LinearProgressIndicator();
+     var name = snapshot.data.documents;
+     name.forEach((DocumentSnapshot ds) => {
+       displayName = ds.data['displayName'],
+       interestss = ds.data['interests']
+     });
+     return buildCard(displayName, interestss);
+   },
+ );
+}
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return new Scaffold(
+      appBar: AppBar(
+      ),
+      body: _buildBody(context, 'u6225609'),
+    );
   }
 }
