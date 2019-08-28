9a24101bc27170e7d0d7a48ec264288733695338
zengtingwei <u6225609@anu.edu.au>
2019-08-28 16:06:50 +1000

    add a test

--- test/db_test.dart
+++ test/db_test.dart
@@ -3,4 +3,8 @@ import 'package:eatwithme/services/db.dart';
 
 void main() {
   final test_db = new DatabaseService();
+  test("test ", (){
+    String id = "u6225609";
+    test_db.streamUser(id);
+  });
 }
